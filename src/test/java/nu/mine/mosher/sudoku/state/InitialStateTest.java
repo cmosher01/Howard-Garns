512f486ee0f34ea18fe80e94c5779ffb98c4139e
Christopher A. Mosher <cmosher01@gmail.com>
2019-07-05 18:34:33 -0400

    working version

---
+++ src/test/java/nu/mine/mosher/sudoku/state/InitialStateTest.java
@@ -0,0 +1,22 @@
+/*
+ * Created on Oct 8, 2005
+ */
+package nu.mine.mosher.sudoku.state;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InitialStateTest
+{
+	/*
+	 * Test method for 'nu.mine.mosher.sudoku.InitialState.createFromString(String)'
+	 */
+	@Test
+	public void testSimpleString()
+	{
+		final String stringRep = "001002003004005006007008009010020030040050060070080090100200300400500600700800900";
+		final InitialState state = InitialState.createFromString(stringRep);
+		assertEquals(stringRep,state.toString());
+	}
+}
